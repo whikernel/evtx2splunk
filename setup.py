0d874c233e4fa3753fd516034b86799ae5204c91
whikernel <whitekernel@whitekernel.fr>
2022-02-18 08:08:16 +0100

    [UPD] Updated package version to match IRIS

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
         'urllib3>=1.26.5',
         'toml==0.10.2',
         'tqdm==4.59.0',
-        'setuptools~=60.7.1',
+        'setuptools==59.6.0',
         'splunk-hec @ git+https://github.com/georgestarcher/Splunk-Class-httpevent.git'
     ]
 
